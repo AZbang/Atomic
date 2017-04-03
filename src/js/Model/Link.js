c1ae1123b54872e59c4a9f2fd8f2ca1b0ec0ae85
AZbang <azbango5@gmail.com>
2017-04-03 14:10:26 +0300

    types Link

--- src/js/Model/Link.js
+++ src/js/Model/Link.js
@@ -9,24 +9,32 @@ class Link {
 
 
 		// create three.js objects
-		this.point1 = new THREE.Vector3(this.atom1.x, this.atom1.y, this.atom1.z);
-		this.point2 = new THREE.Vector3(this.atom2.x, this.atom2.y, this.atom2.z);
 
-		this.geometry = new THREE.TubeGeometry(
-			new THREE.SplineCurve3([this.point1, this.point2]),
-			10, 2, 8, false
-		);
-		this.mesh = new THREE.Object3D();
-		this.mesh.add(new THREE.Mesh(
-			this.geometry,
-			new THREE.MeshPhongMaterial({
-				color: 0xB0B0B0,
-				emissive: 0x7B7B7B,
-				side: THREE.DoubleSide,
-				shading: THREE.FlatShading
-			})
-		));
-		this.molecula.stage.add(this.mesh);
+		this.tubes = new THREE.Group();
+		this.molecula.stage.add(this.tubes);
+
+		for(let i = 0; i < type; i++) {
+
+			let p1 = new THREE.Vector3(this.atom1.x, this.atom1.y, this.atom1.z+i*(6/type)-(2/type)*(type-1));
+			let p2 = new THREE.Vector3(this.atom2.x, this.atom2.y, this.atom2.z+i*(6/type)-(2/type)*(type-1));
+
+			let mesh = new THREE.Object3D();
+			let geometry = new THREE.TubeGeometry(
+				new THREE.SplineCurve3([p1, p2]),
+				12, 2/type
+			);
+
+			mesh.add(new THREE.Mesh(
+				geometry,
+				new THREE.MeshPhongMaterial({
+					color: 0xB0B0B0,
+					emissive: 0x7B7B7B,
+					side: THREE.DoubleSide,
+					shading: THREE.FlatShading
+				})
+			));
+			this.tubes.add(mesh);
+		}
 	}
 }
 
