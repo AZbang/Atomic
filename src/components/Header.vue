74be17e67df1a13a0356ebab66aec6b76eee8990
AZbang <azbango5@gmail.com>
2018-02-25 00:12:28 +0300

    add search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,34 @@
 <template lang="html">
   <v-toolbar app color="blue">
-   <!-- <v-toolbar-side-icon class="white--text"></v-toolbar-side-icon> -->
-   <v-toolbar-title class="white--text">{{this.$route.name}}</v-toolbar-title>
-   <v-spacer></v-spacer>
-   <v-btn icon class="white--text">
-     <v-icon>search</v-icon>
-   </v-btn>
- </v-toolbar>
+    <v-toolbar-title v-show="!isSearch" class="white--text">{{this.$route.name}}</v-toolbar-title>
+    <v-spacer v-show="!isSearch"></v-spacer>
+    <v-text-field
+      ref="search"
+      style="margin-left: 6px; box-shadow: none;"
+      v-show="isSearch"
+      v-model="req"
+      @blur="isSearch = false"
+      solo placeholder="Search substance">
+    </v-text-field>
+    <v-btn icon class="white--text" @click="toggleSearch">
+      <v-icon>search</v-icon>
+    </v-btn>
+  </v-toolbar>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        req: '',
+        isSearch: false
+      }
+    },
+    methods: {
+      toggleSearch() {
+        this.isSearch = !this.isSearch;
+        this.$refs.search.$el.focus();
+      }
+    }
+  }
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template lang="html">
   <v-bottom-nav app :value="true" :active.sync="$route.meta" color="white">
-    <v-btn href="#/types" flat color="blue" value="substances">
+    <v-btn to="/types" flat color="blue" value="substances">
       <span>Substances</span>
       <v-icon>book</v-icon>
     </v-btn>
-    <v-btn href="#/favorites" flat color="blue" value="favorites">
+    <v-btn to="/favorites" flat color="blue" value="favorites">
       <span>Favorites</span>
       <v-icon>favorite</v-icon>
     </v-btn>
-    <v-btn href="#/learn" flat color="blue" value="learn">
+    <v-btn to="/learn" flat color="blue" value="learn">
       <span>Learn</span>
       <v-icon>extension</v-icon>
     </v-btn>
